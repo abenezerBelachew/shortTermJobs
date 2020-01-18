a210c6ec0fa3a01d749ba8455f2b668de05217f5
nafisa-hasan <53453175+nafisa-hasan@users.noreply.github.com>
2020-01-18 14:53:20 -0700

    Update views.py

--- Jobs/views.py
+++ Jobs/views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-  #Get an HttpRequest - the request parameter
-  #perform operations
-  #Return Http Response
-  return HttpResponse('')
 
 # Create your views here.
